c5bbbfc939b0c9df40dd82d1d638500be1d659d7
Jaime Jacobo <39837430+JaimeJacobo@users.noreply.github.com>
2020-05-18 16:25:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const chalk = require('chalk');
 require('dotenv').config()
-const database = require('./conf'); 
+// const database = require('./conf'); 
 const bodyParser = require('body-parser')
 
 const app = express();
@@ -12,37 +12,37 @@ app.use(bodyParser.json())
 
 
 //GET route to home page
-app.get('/', (req, res)=>{
-    res.send('Connected')
-})
+// app.get('/', (req, res)=>{
+//     res.send('Connected')
+// })
 
 //Route to connect checkpoint database /playlist and /track
- app.get('/contacts', (req, res)=>{
-    database.query('SELECT * FROM contact', (error, results)=>{
-        if(error){ 
-            console.log(chalk.red.inverse('There is an error with GET /contact'));
-            res.send(error)
-        } else{
-            console.log(chalk.green.inverse('The route GET /contact was called successfully'));
-            res.send(results)
-        }
-    })
-}) 
+//  app.get('/contacts', (req, res)=>{
+//     database.query('SELECT * FROM contact', (error, results)=>{
+//         if(error){ 
+//             console.log(chalk.red.inverse('There is an error with GET /contact'));
+//             res.send(error)
+//         } else{
+//             console.log(chalk.green.inverse('The route GET /contact was called successfully'));
+//             res.send(results)
+//         }
+//     })
+// }) 
 
 
 
 //1.  POST - create a new playlist 
 //201 status code - created
 //400 status code - Bad Request
-app.post('/newContact', (req, res)=>{
-    database.query('INSERT INTO contact SET ?', req.body, (error, results)=>{
-      if(!error){
-        res.status(201).json(req.body);
-      } else {
-        res.status(400).send(error);
-      }
-    })
-  })
+// app.post('/newContact', (req, res)=>{
+//     database.query('INSERT INTO contact SET ?', req.body, (error, results)=>{
+//       if(!error){
+//         res.status(201).json(req.body);
+//       } else {
+//         res.status(400).send(error);
+//       }
+//     })
+//   })
 
 
 
